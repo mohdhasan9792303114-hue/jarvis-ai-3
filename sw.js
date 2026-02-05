97ef6fd596893f3fa7edcd1c35843424e6145bcf
mohdhasan9792303114-hue <mohdhasan9792303114@gmail.com>
2026-02-06 04:23:10 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,23 +1,21 @@
-const CACHE_NAME = "jarvis-cache-v1";
+const CACHE_NAME = "jarvis-ai-cache-v1";
 
 const urlsToCache = [
-  "/jarvis-ai-3/",
-  "/jarvis-ai-3/index.html",
-  "/jarvis-ai-3/manifest.json"
+  "./",
+  "./index.html",
+  "./manifest.json"
 ];
 
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
   );
 });
